4ef5182c34e8de329f7e43965e901dad572490ab
san-calibre <67945191+san07364@users.noreply.github.com>
2021-10-05 18:14:26 +0530

    logo changed

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -6,16 +6,8 @@ const IconLoader = () => (
     <g>
       <g id="B" transform="translate(11.000000, 5.000000)"></g>
       <path
-        stroke="currentColor"
-        strokeWidth="5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M 50, 5
-                  L 11, 27
-                  L 11, 72
-                  L 50, 95
-                  L 89, 73
-                  L 89, 28 z"
+        d="M7.28125 29.3594L29.2891 38.2539V45.1445L0.53125 31.8203V26.6875L29.2891 13.3984V20.2891L7.28125 29.3594ZM40.8555 56.3945H35.2656L56.6406 0.8125H62.1953L40.8555 56.3945ZM92.2539 29.1484L69.0156 20.043V13.3633L99.0391 26.6523V31.7852L69.0156 45.1094V38.3594L92.2539 29.1484Z"
+        fill="currentColor"
       />
     </g>
   </svg>
