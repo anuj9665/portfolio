a0108494005c496008f3739aeaff5b4a997fdddd
san-calibre <67945191+san07364@users.noreply.github.com>
2021-10-11 13:59:06 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ module.exports = {
     title: 'Sandeep Kumar',
     description:
       'Sandeep Kumar is a software engineer who specializes in building (and occasionally designing) exceptional crossplatform applications.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://codewithsan.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@san07364',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'SandeepKumar',
+        short_name: 'SandeepKumar',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
